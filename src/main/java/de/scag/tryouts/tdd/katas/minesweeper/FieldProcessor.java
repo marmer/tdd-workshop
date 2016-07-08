8ec2942c256617c4c54e0ce0465bff71850ebb01
MarMer <1055445+marmer@users.noreply.github.com>
2016-07-08 17:31:37 +0200

    refactoring

--- src/main/java/de/scag/tryouts/tdd/katas/minesweeper/FieldProcessor.java
+++ src/main/java/de/scag/tryouts/tdd/katas/minesweeper/FieldProcessor.java
@@ -8,23 +8,33 @@ public class FieldProcessor {
     public int[][] process(final int[][] unprocessedField) {
         final int[][] processedField = new int[unprocessedField.length][];
 
-        for (int y = 0; y < unprocessedField.length; y++) {
-            processedField[y] = new int[unprocessedField[y].length];
-
-            for (int x = 0; x < unprocessedField[y].length; x++) {
-                if (unprocessedField[y][x] == MiningFileService.FIELD_MINE) {
-                    processedField[y][x] = MiningFileService.FIELD_MINE;
-
-                    continue;
-                }
-
-                if ((unprocessedField[y][x + 1] == MiningFileService.FIELD_MINE) &&
-                        ((x + 1) <= unprocessedField.length)) {
-                    processedField[y][x] = 1;
-                }
-            }
+        for (int line = 0; line < unprocessedField.length; line++) {
+            process(unprocessedField, processedField, line);
         }
 
         return processedField;
+    }
+
+    private void process(final int[][] unprocessedField,
+        final int[][] processedField,
+        final int line) {
+        processedField[line] = new int[unprocessedField[line].length];
+
+        for (int col = 0; col < unprocessedField[line].length; col++) {
+            if (isMine(unprocessedField[line][col])) {
+                processedField[line][col] = MiningFileService.FIELD_MINE;
+
+                continue;
+            }
+
+            if (((col + 1) <= unprocessedField.length) &&
+                    (isMine(unprocessedField[line][col + 1]))) {
+                processedField[line][col] = 1;
+            }
+        }
+    }
+
+    private boolean isMine(final int potentialMine) {
+        return potentialMine == MiningFileService.FIELD_MINE;
     }
 }
