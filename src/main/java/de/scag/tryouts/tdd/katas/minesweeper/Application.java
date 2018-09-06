78f2034fc582e795ee5244b3a1348b2315dc402d
MarMer <1055445+marmer@users.noreply.github.com>
2018-09-06 17:40:05 +0200

    tooling upgrade

--- src/main/java/de/scag/tryouts/tdd/katas/minesweeper/Application.java
+++ src/main/java/de/scag/tryouts/tdd/katas/minesweeper/Application.java
@@ -1,8 +1,8 @@
 package de.scag.tryouts.tdd.katas.minesweeper;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 
 @SpringBootApplication
 public class Application {
