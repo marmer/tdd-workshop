2ed017817918aa32cca59aadf8f14e39691b614d
MarMer <1055445+marmer@users.noreply.github.com>
2018-09-06 17:41:43 +0200

    tooling upgrade

--- src/main/java/de/scag/tryouts/tdd/katas/minesweeper/CommandLineRunnerConfig.java
+++ src/main/java/de/scag/tryouts/tdd/katas/minesweeper/CommandLineRunnerConfig.java
@@ -1,20 +1,14 @@
 package de.scag.tryouts.tdd.katas.minesweeper;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.boot.CommandLineRunner;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 
 @Configuration
-@Profile("!test") // Gilt für alle Profile außer test
-// Alterantive ohhne Profil. Hier müsste im Test noch ein property gesetzt werden. Siehe
-// MinesweeperITest:
-// @ConditionalOnProperty(prefix = "job.autorun", name = "enabled", havingValue = "true",
-// matchIfMissing = true)
+@Profile("!test")
 public class CommandLineRunnerConfig {
     @Autowired
     private Minesweeper minesweeper;
--- src/test/java/de/scag/tryouts/tdd/katas/minesweeper/MinesweeperITest.java
+++ src/test/java/de/scag/tryouts/tdd/katas/minesweeper/MinesweeperITest.java
@@ -1,56 +1,32 @@
 package de.scag.tryouts.tdd.katas.minesweeper;
 
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.is;
-
-import static org.junit.Assert.assertThat;
-
 import org.junit.Rule;
 import org.junit.Test;
-
 import org.junit.rules.TemporaryFolder;
-
 import org.junit.runner.RunWith;
-
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.boot.test.context.SpringBootTest;
-
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.File;
-
 import java.net.URISyntaxException;
-
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-
 import java.util.List;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
-// Spring 1.3 Weg
-// @RunWith(SpringJUnit4ClassRunner.class)
-// @SpringApplicationConfiguration(
-// { Minesweeper.class, FieldProcessor.class, MiningFileService.class }
-// )
 
-// Spring 1.5 Weg
 @RunWith(SpringRunner.class)
-@SpringBootTest( /*properties = {"job.autorun.enabled=false"} wenn Bedingung an Config ist*/)
-
-// 1. Möglichkeit zusammen mit @Profile("!test") (siehe Klasse Config) den Commandlinerunner zu
-// unterdrücken. In Spring 1.3 nicht nötig
-@ActiveProfiles("test")
-// Der Annotation SpringBootTest können auch ausgewählte Klassen mitgegeben werden. In dem Fall
-// werden ausschließlich diese verwendet
+@SpringBootTest
 public class MinesweeperITest {
-    @Autowired
-    private Minesweeper classUnderTest;
-
     @Rule
     public TemporaryFolder tmp = new TemporaryFolder();
+    @Autowired
+    private Minesweeper classUnderTest;
 
     @Test
     public void testRun_DateiMitGueltigemMinenfeldGegeben_SollteDateiMitAufgeloestemMinenfeldErzeugen()
