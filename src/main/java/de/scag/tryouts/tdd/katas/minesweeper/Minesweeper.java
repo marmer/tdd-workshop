5506df9f8d3c2736a388d1d8233adae62dd44568
MarMer <1055445+marmer@users.noreply.github.com>
2018-09-06 21:52:42 +0200

    * German description removed * English description added in readme.md * obsolete comments removed * Field injection replaced by constructor injection

--- src/main/java/de/scag/tryouts/tdd/katas/minesweeper/Minesweeper.java
+++ src/main/java/de/scag/tryouts/tdd/katas/minesweeper/Minesweeper.java
@@ -3,39 +3,6 @@ package de.scag.tryouts.tdd.katas.minesweeper;
 import org.springframework.stereotype.Service;
 
 
-/**
- * Spezifikation grob: Ein aus einer Datei gelesenes Mienenfeld soll an allen
- * minenfreien Stellen mit Informationen (Zahl) versehen werden, wie viele Minen
- * die Benachbarten Felder haben und anschließend in einer Datei anderen Datei
- * gespeichert werden.
- *
- * <p>Weitere Details:</p>
- *
- * <ol>
- *   <li>Ein Feld mit einer Miene wird in der Quell und der Zieldatei mit
- *     folgendem Symbol dargestellt: *</li>
- *   <li>Ein leeres Feld wird in der Eingabedatei durch einen Punkt
- *     dargestellt</li>
- *   <li>Ein leeres Feld in der Ausgabedatei wird mit einer Ziffer dargestellt
- *   </li>
- *   <li>Die Ziffer eines leeren Felds in der Ausgabedatei gibt an, wieviele
- *     Mienen in den umliegenden Feldern vorhanden sind</li>
- *   <li>Als umliegendes Feld gelten ebenso horizontale wie vertikale Nachbarn
- *   </li>
- *   <li>Ein Mienenfeld ist stets quadratisch.</li>
- *   <li>Alle Zeilen eines Mienenfelds sind gleich lang</li>
- *   <li>Alle Spalten eines Mienenfelds sind gleich lang</li>
- *   <li>Die Größe eines Minenfeldes ist beliebig</li>
- *   <li>Bei Problemen mit den Eingabedaten soll das Programm mit einer
- *     angemessenen Fehlerbeschreibung enden.</li>
- *   <li>Der erste Parameter der Main Methode beschreibt die Eingabedatei</li>
- *   <li>Der zweite Parameter der Main Methode beschreibt die Ausgabedatei</li>
- *   <li>Beide Parameter sind pflicht</li>
- * </ol>
- *
- * @author mertinat
- * @since  07.07.2016
- */
 @Service
 public class Minesweeper {
     public void run(final String... args) throws Exception {
