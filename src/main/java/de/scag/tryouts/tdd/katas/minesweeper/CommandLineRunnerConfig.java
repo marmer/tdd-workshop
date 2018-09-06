ec27bf7fd69ac74095fc10e19f01a0adfd0c94f3
MarMer <1055445+marmer@users.noreply.github.com>
2018-09-06 21:52:06 +0200

    * German description removed * English description added in readme.md * obsolete comments removed * Field injection replaced by constructor injection

--- src/main/java/de/scag/tryouts/tdd/katas/minesweeper/CommandLineRunnerConfig.java
+++ src/main/java/de/scag/tryouts/tdd/katas/minesweeper/CommandLineRunnerConfig.java
@@ -1,23 +1,17 @@
 package de.scag.tryouts.tdd.katas.minesweeper;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 
 @Configuration
-@Profile("!test") // Gilt für alle Profile außer test
-// Alterantive ohhne Profil. Hier müsste im Test noch ein property gesetzt werden. Siehe
-// MinesweeperITest:
-// @ConditionalOnProperty(prefix = "job.autorun", name = "enabled", havingValue = "true",
-// matchIfMissing = true)
+@Profile("!test")
+@RequiredArgsConstructor
 public class CommandLineRunnerConfig {
-    @Autowired
-    private Minesweeper minesweeper;
+    private final Minesweeper minesweeper;
 
     @Bean
     public CommandLineRunner commandLineRunner() {
