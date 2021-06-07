2d09de14785601144178ced62a18a2c90c0e1d39
mariano.mertinat <Mariano.Mertinat@gematik.de>
2021-06-07 17:36:30 +0200

    Initialer Beispieltestfall

--- src/test/java/io/github/marmer/OhceTest.java
+++ src/test/java/io/github/marmer/OhceTest.java
@@ -1,7 +1,13 @@
 package io.github.marmer;
 
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 import java.util.function.Consumer;
 import java.util.function.Supplier;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -18,5 +24,19 @@ class OhceTest {
 
     @Mock
     private Consumer<String> outputSupplier;
-    
+
+
+    @Test
+    @DisplayName("Die Anwendung sollte sich durch ein End-Kommando beenden")
+    @SneakyThrows
+    void start_DieAnwendungSollteSichDurchEinEndKommandoBeenden() {
+        // Preparation
+        when(inputSupplier.get()).thenReturn("Stop!");
+
+        // Execution
+        underTest.start("Frank");
+
+        // Assertion
+        verify(outputSupplier).accept("Adios Frank");
+    }
 }
